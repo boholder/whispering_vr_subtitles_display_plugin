ec427a44229292b62e7a9df7f09cb7cf3f5152e3
boholder <bottleholder@anche.no>
2026-01-10 22:29:47 +0800

    openvr v2.12.14

---
+++ scripts/update_openvr.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+OPENVR_VERSION_TAG=$1
+
+cd third
+git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/ValveSoftware/openvr.git
+cd openvr
+git sparse-checkout set --no-cone /bin /headers /lib
+git checkout "$OPENVR_VERSION_TAG"
+rm -rf .git
